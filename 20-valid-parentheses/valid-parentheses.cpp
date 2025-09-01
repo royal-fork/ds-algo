fa80211d3714c4cf8031b0e47c3fa0ba9c8c7fdb
Raghav sood <54942697+royal-fork@users.noreply.github.com>
2025-09-01 21:04:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (64.02%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto it: s){
+            if(it=='(' || it=='[' || it=='{'){
+                st.push(it);
+            }else{
+                if(st.empty()){
+                    return false;
+                }
+                char top =st.top();
+                st.pop();
+                 if (it  == ')' && top != '(') return false;
+                if (it == ']' && top != '[') return false;
+                if (it == '}' && top != '{') return false;
+
+            }
+        }
+        return st.empty();
+
+        
+    }
+};
