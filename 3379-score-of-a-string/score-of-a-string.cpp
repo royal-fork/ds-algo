85b1d9535a716aa7f3899038843e88190203447f
Raghav sood <54942697+royal-fork@users.noreply.github.com>
2025-09-02 11:40:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (28.41%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -0,0 +1,32 @@
+typedef int skibidi;
+typedef string rizz;
+using edging = vector<skibidi>;
+using muhahaha = vector<edging>;
+istream& gooning = cin;
+ostream& ohio = cout;
+#define fanumtax for
+#define looksmaxxing push_back
+#define aura pop_back
+#define sigma max_element 
+#define apple begin
+#define endofthegrind end
+#define crashout return
+#define ishowspeed class
+#define goon public
+#define alpha size
+#define zeroaura 0
+#define hawk if
+#define tuah else
+#define superidol abs
+#define Tob_Tobi_Tob_Tob_Tobi_Tob 1
+
+ishowspeed Solution {
+goon:
+    skibidi scoreOfString(rizz baby_gronk) {
+        skibidi grind = zeroaura;
+        fanumtax (skibidi diddy = zeroaura;diddy<baby_gronk.alpha()-Tob_Tobi_Tob_Tob_Tobi_Tob;diddy++) {
+            grind += superidol(baby_gronk[diddy]-baby_gronk[diddy+Tob_Tobi_Tob_Tob_Tobi_Tob]);
+        }
+        crashout grind;
+    }
+};
