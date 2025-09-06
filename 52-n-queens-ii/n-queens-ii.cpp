eb4f72f8ede8f00168da075fa54919523168ac25
Raghav sood <54942697+royal-fork@users.noreply.github.com>
2025-09-06 09:26:53 +0530

    Time: 2 ms (62.61%) | Memory: 8.2 MB (71.28%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,7 +1,52 @@
 class Solution {
 public:
+    int count = 0;
+
+    bool safe(int row, int col, vector<string>& board, int n) {
+        int dupr = row;
+        int dupc = col;
+
+        // upper-left diagonal
+        while (dupr >= 0 && dupc >= 0) {
+            if (board[dupr][dupc] == 'Q') return false;
+            dupr--;
+            dupc--;
+        }
+
+        // left row
+        dupr = row; dupc = col;
+        while (dupc >= 0) {
+            if (board[dupr][dupc] == 'Q') return false;
+            dupc--;
+        }
+
+        // lower-left diagonal
+        dupr = row; dupc = col;
+        while (dupr < n && dupc >= 0) {
+            if (board[dupr][dupc] == 'Q') return false;
+            dupc--;
+            dupr++;
+        }
+        return true;
+    }
+
+    void solver(int col, vector<string>& board, int n) {
+        if (col == n) {
+            count++;
+            return;
+        }
+        for (int row = 0; row < n; row++) {
+            if (safe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                solver(col + 1, board, n);
+                board[row][col] = '.'; 
+            }
+        }
+    }
+
     int totalNQueens(int n) {
-        vector<int> answers = {0, 1, 0, 0, 2, 10, 4, 40, 92, 352};
-        return answers[n];
+        vector<string> board(n, string(n, '.'));
+        solver(0, board, n);
+        return count;
     }
 };
