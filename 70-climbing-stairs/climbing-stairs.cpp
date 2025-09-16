c693dd5de7039aa8beacf22fad70250a32a01fbe
Raghav sood <54942697+royal-fork@users.noreply.github.com>
2025-09-16 21:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (87.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n==0||n==1){
-            return 1;
+    int solve(int n,vector<int>& dp){
+        if(n<=2) return n;
+        if(dp[n] != -1) return dp[n];
 
-        }
-        vector<int> dp(n+1);
-        dp[0]=dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
         return dp[n];
 
-        
+    }
+    int climbStairs(int n) {
+    // brute force krdia shi
+    // if(n<=2) return n;
+
+    // return climbStairs(n-1)+climbStairs(n-2);
+        vector<int>dp(46,-1);
+       return  solve(n,dp);
     }
 };
