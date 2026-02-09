a3312c26a019453de8174991c0a1bb9eb1a9ffe4
Raghav sood <54942697+royal-fork@users.noreply.github.com>
2026-02-09 20:01:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (15.96%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
+        sort(strs.begin(),strs.end());
+        int n=strs.size();
+        string  first=strs[0];
+        string  last=strs[n-1];
+        string res="";
+        int i=0;
 
-        // Sort the strings lexicographically
-        sort(strs.begin(), strs.end());
-
-        string first = strs[0];
-        string last = strs[strs.size() - 1];
-        string res = "";
-
-        for (int i = 0; i < first.size(); i++) {
-            if (first[i] == last[i]) {
-                res += first[i];
-            } else {
+        while(i<first.size() && i<last.size()){
+            if(first[i]==last[i]){
+                res.push_back(first[i]);
+                i++;
+            }else{
                 break;
             }
         }
-
         return res;
+        
     }
 };
