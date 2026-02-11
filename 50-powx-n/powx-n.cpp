2e701294d5261b4d57844e08d13082e5f7ee0b61
Raghav sood <54942697+royal-fork@users.noreply.github.com>
2026-02-12 00:16:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.51%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
 
-    double solver(double x, long long n) {
-        if (n == 0) return 1;
+    double solver(double x,long long n){
+        //base case for 0 values
+        if(n==0)   return 1;
+        if(n<0)    return solver(1/x,-n);
+        if(n%2==0) return solver(x*x,n/2);
+        // if(n%2==1) 
+        return x*solver(x*x,(n-1)/2);
 
-        if (n % 2 == 0)
-            return solver(x * x, n / 2);
-        else
-            return x * solver(x * x, (n - 1) / 2);
     }
-
     double myPow(double x, int n) {
-        long long nn = n;
+       
+        long long nn=n;
 
-        if (nn < 0) {
-            x = 1 / x;
-            nn = -nn;
-        }
-
-        return solver(x, nn);
+        return solver(x,nn);
+        
     }
 };
